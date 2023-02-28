8353f469c17af3eb5bcda3e68716bb3a59a9d4d3
Minh Thuan <minhthuanst01@gmail.com>
2023-02-28 09:02:49 +0700

    feat: player moving

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  [SerializeField] private Animator Ani;
 
-    // Update is called once per frame
-    void Update()
+  [SerializeField] private int combo;
+  [SerializeField] private bool attack;
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    Ani = GetComponent<Animator>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Combos();
+  }
+
+  private void Combos()
+  {
+    if (Input.GetKeyDown(KeyCode.J) && !attack)
     {
-        
+      attack = true;
+      Ani.SetTrigger("" + combo);
+
     }
+  }
+  [SerializeField]
+  private void Start_Combo()
+  {
+    attack = false;
+    if (combo < 3)
+    {
+      combo++;
+    }
+  }
+  [SerializeField]
+  private void Finish_Ani()
+  {
+    attack = false;
+    combo = 0;
+  }
 }
