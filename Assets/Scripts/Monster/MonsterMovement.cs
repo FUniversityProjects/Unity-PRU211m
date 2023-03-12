e3b7a9e5cb88368842938ab1f5eaae144fcf5069
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-03-12 19:43:29 +0700

    fix: change assets for monster

--- Assets/Scripts/Monster/MonsterMovement.cs
+++ Assets/Scripts/Monster/MonsterMovement.cs
@@ -4,53 +4,50 @@ using UnityEngine;
 
 public class MonsterMovement : MonoBehaviour
 {
-  [Range(4, 9)]
-  [SerializeField] private float moveSpeed;
-  [SerializeField] private Transform leftPoint, rightPoint;
-  [SerializeField] private SpriteRenderer spriteBody;
-  [SerializeField] private SpriteRenderer spriteHeader;
-  private bool _movingRight;
-  private Animator _anim;
-  private Rigidbody2D _body;
+    [Range(2, 6)]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private Transform leftPoint, rightPoint;
+    [SerializeField] private SpriteRenderer sprite;
+    private bool _movingRight;
+    private Animator _anim;
+    private Rigidbody2D _body;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    _anim = GetComponent<Animator>();
-    _body = GetComponent<Rigidbody2D>();
-
-    leftPoint.parent = null;
-    rightPoint.parent = null;
-
-    _movingRight = false;
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (_movingRight)
+    // Start is called before the first frame update
+    void Start()
     {
-      _body.velocity = new Vector2(moveSpeed, _body.velocity.y);
+        _anim = GetComponent<Animator>();
+        _body = GetComponent<Rigidbody2D>();
 
-      spriteBody.flipX = true;
-      spriteHeader.flipX = true;
+        leftPoint.parent = null;
+        rightPoint.parent = null;
 
-      if (transform.position.x > rightPoint.position.x)
-      {
         _movingRight = false;
-      }
     }
-    else
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
-      _body.velocity = new Vector2(-moveSpeed, _body.velocity.y);
+        if (_movingRight)
+        {
+            _body.velocity = new Vector2(moveSpeed, _body.velocity.y);
 
-      spriteBody.flipX = false;
-      spriteHeader.flipX = false;
+            sprite.flipX = false;
 
-      if (transform.position.x < leftPoint.position.x)
-      {
-        _movingRight = true;
-      }
+            if (transform.position.x > rightPoint.position.x)
+            {
+                _movingRight = false;
+            }
+        }
+        else
+        {
+            _body.velocity = new Vector2(-moveSpeed, _body.velocity.y);
+
+            sprite.flipX = true;
+
+            if (transform.position.x < leftPoint.position.x)
+            {
+                _movingRight = true;
+            }
+        }
     }
-  }
 }
