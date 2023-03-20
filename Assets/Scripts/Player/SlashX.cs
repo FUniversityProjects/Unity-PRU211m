72f7572d9a99be611ed472c330ea2808c619c5f6
Huenhan2000 <nguyenhoanghuenhan2000@gmail.com>
2023-03-20 22:42:47 +0700

    feat: main menu, sound

--- Assets/Scripts/Player/SlashX.cs
+++ Assets/Scripts/Player/SlashX.cs
@@ -4,67 +4,67 @@ using UnityEngine;
 
 public class SlashX : MonoBehaviour
 {
-  [SerializeField] private float speed;
-  private float direction;
-  private bool hit;
-  private Animator animator;
-  private PolygonCollider2D polygonCollider2D;
-  [SerializeField] private float deactive = 0;
+    [SerializeField] private float speed;
+    private float direction;
+    private bool hit;
+    private Animator animator;
+    private PolygonCollider2D polygonCollider2D;
+    [SerializeField] private float deactive = 0;
 
 
-  private void Awake()
-  {
-    animator = GetComponent<Animator>();
-    polygonCollider2D = GetComponent<PolygonCollider2D>();
-  }
-
-  private void Update()
-  {
-    if (hit) return;
-    float movementSpeed = (speed * Time.deltaTime * direction);
-    transform.Translate(movementSpeed, 0, 0);
-  }
-  private void OnTriggerEnter2D(Collider2D collision)
-  {
-    hit = true;
-    polygonCollider2D.enabled = false;
-    Deactivate();
-  }
-
-  public void SetDerection(float _direction)
-  {
-    direction = _direction;
-    gameObject.SetActive(true);
-    hit = false;
-    polygonCollider2D.enabled = true;
-
-    // float localScaleX = PlayerMovement.instance.transform.localScale.x;
-    float localScaleX = transform.localScale.x;
-    if (Mathf.Sign(localScaleX) != _direction)
+    private void Awake()
     {
-      localScaleX = -localScaleX;
+        animator = GetComponent<Animator>();
+        polygonCollider2D = GetComponent<PolygonCollider2D>();
     }
 
-    transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
-  }
-
-  private void Deactivate()
-  {
-    gameObject.SetActive(false);
-  }
-  private void FixedUpdate()
-  {
-
-    if (deactive >= 2)
+    private void Update()
     {
-      polygonCollider2D.enabled = false;
-      Deactivate();
-      deactive = 0;
+        if (hit) return;
+        float movementSpeed = (speed * Time.deltaTime * direction);
+        transform.Translate(movementSpeed, 0, 0);
     }
-    else
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-      deactive += Time.deltaTime;
+        hit = true;
+        polygonCollider2D.enabled = false;
+        Deactivate();
     }
 
-  }
+    public void SetDerection(float _direction)
+    {
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        polygonCollider2D.enabled = true;
+
+        // float localScaleX = PlayerMovement.instance.transform.localScale.x;
+        float localScaleX = transform.localScale.x;
+        if (Mathf.Sign(localScaleX) != _direction)
+        {
+            localScaleX = -localScaleX;
+        }
+
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+    private void FixedUpdate()
+    {
+
+        if (deactive >= 2)
+        {
+            polygonCollider2D.enabled = false;
+            Deactivate();
+            deactive = 0;
+        }
+        else
+        {
+            deactive += Time.deltaTime;
+        }
+
+    }
 }
