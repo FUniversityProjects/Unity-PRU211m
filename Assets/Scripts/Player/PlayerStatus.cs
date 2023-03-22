2d5b07a4e18c1d6a51e44f3a932cc94b33b161b3
HxThanh <107059286+HxThanh@users.noreply.github.com>
2023-03-22 09:28:27 -0500

    respawn and checkpoint

--- Assets/Scripts/Player/PlayerStatus.cs
+++ Assets/Scripts/Player/PlayerStatus.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Scripts.Interfaces;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour, IDamage
@@ -9,13 +11,16 @@ public class PlayerStatus : MonoBehaviour, IDamage
   [SerializeField] private float hp;
   [SerializeField] private Animator Ani;
   public HealthBar healthBar;
-  //public PlayerMovement player;
+  public bool isDead = false;
+  private Vector3 respawnPoint;
+
 
   void Start()
   {
     hp = maxHP;
     healthBar.SetHealth(hp, maxHP);
-  }
+    respawnPoint = transform.position;
+    }
   public void TakeDamage(float damage)
   {
     hp -= damage;
@@ -26,23 +31,49 @@ public class PlayerStatus : MonoBehaviour, IDamage
       StartCoroutine(Die());
     }
   }
+    public void AddHealth(float _value)
+    {
+        hp = Mathf.Clamp(hp + _value, 0, maxHP);
+    }
+    
   public IEnumerator Die()
   {
     Debug.Log("Player died! ");
     //Die animation
     Ani.SetBool("isDead", true);
     //Disable Enemy
-    yield return new WaitForSeconds(1.3f);
+    yield return new WaitForSeconds(0.8f);
 
-    Destroy(gameObject);
+    isDead = true;
+    Ani.SetBool("isDead" ,false);
+    transform.position = respawnPoint;
+        AddHealth(maxHP);
+
+        ; 
+
     // GetComponent<Collider2D>().enabled = false;
     // GetComponent<Renderer>().enabled = false;
     // this.enabled = false;
 
   }
+    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "FallDectector")
+        {
+            transform.position = respawnPoint;
+        }
+        else if (collision.tag == "Checkpoint")
+        {
+            Debug.Log("Checkpoint");
+            respawnPoint = transform.position;
+        }
+    }
 
-  public void TakeHit()
+
+    public void TakeHit()
   {
     throw new System.NotImplementedException();
   }
+
 }
